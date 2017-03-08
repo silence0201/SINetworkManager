77bbdb605302a35ec7ed5fca99488b78bb27c5f7
Silence <374619540@qq.com>
2017-03-08 18:34:36 +0800

    Initial Commit

---
+++ SINetworkManagerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SINetworkManagerDemo
+//
+//  Created by 杨晴贺 on 2017/3/8.
+//  Copyright © 2017年 Silence. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SINetworkManagerDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SINetworkManagerDemo
+//
+//  Created by 杨晴贺 on 2017/3/8.
+//  Copyright © 2017年 Silence. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
