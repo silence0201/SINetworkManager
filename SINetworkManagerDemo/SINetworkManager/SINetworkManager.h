f95c4525f676297944da2284e900069cd6046a01
Silence <374619540@qq.com>
2017-03-08 21:34:54 +0800

    添加网络状态代码

--- SINetworkManagerDemo/SINetworkManager/SINetworkManager.h
+++ SINetworkManagerDemo/SINetworkManager/SINetworkManager.h
@@ -8,6 +8,20 @@
 
 #import <Foundation/Foundation.h>
 
+
+NS_ASSUME_NONNULL_BEGIN
+
+///网络状态变化时发出通知
+FOUNDATION_EXPORT NSString *const SINetworkStatusDidChangeNotification ;
+
+typedef NS_ENUM(NSInteger,SINetworkStatusType) {
+    SINetworkStatusUnknow = 0,          ///< 未知网络
+    SINetworkStatusNotReachable,        ///< 无网络
+    SINetworkStatusReachableViaWWAN,    ///< 蜂窝网络
+    SINetworkStatusReachableViaWiFi,    ///< wifi
+};
+
+
 typedef NS_ENUM(NSInteger,SIRequestSerializerType){
     SIRequestSerializerHTTP = 0, ///< 请求的数据格式为二进制数据
     SIRequestSerializerJSON     ///< 请求数据为JSON格式
@@ -19,9 +33,10 @@ typedef NS_ENUM(NSInteger,SIResponseSerializerType){
     SIResponseSerializerXML       ///< 返回的数据为XML
 };
 
-NS_ASSUME_NONNULL_BEGIN
 
-typedef void(^SIRequestCacheBlock)(id responseCache); ///>  缓存Block
+
+typedef void(^SIRequestCacheBlock)(id responseCache) ; ///>  缓存Block
+typedef void(^SINetworkStatusBlock)(SINetworkStatusType status) ; ///> 网络状态发生改变
 
 @interface SINetworkCache : NSObject
 
@@ -38,25 +53,54 @@ typedef void(^SIRequestCacheBlock)(id responseCache); ///>  缓存Block
 
 @interface SINetworkConfig : NSObject
 
-@property (nonatomic,copy) NSString *baseURL ; ///> 根地址
-@property (nonatomic,copy) NSDictionary *commonParas ; ///> 公共参数
-@property (nonatomic,assign) NSTimeInterval timeoutInterval ; ///> 超时时间,默认为30s
+/// 根地址
+@property (nonatomic,copy) NSString *baseURL ;
+/// 公共参数
+@property (nonatomic,copy) NSDictionary *commonParas ;
+/// 超时时间,默认为30s
+@property (nonatomic,assign) NSTimeInterval timeoutInterval ;
 
-@property (nonatomic,assign) SIRequestSerializerType requestSerializerType ;   ///> 请求数据类型,默认是二进制类型
-@property (nonatomic,assign) SIResponseSerializerType responseSerializerType ;  ///> 返回数据类型
+/// 请求数据类型,默认是二进制类型
+@property (nonatomic,assign) SIRequestSerializerType requestSerializerType ;
+/// 返回数据类型,默认是二进制
+@property (nonatomic,assign) SIResponseSerializerType responseSerializerType ;
 
-@property (nonatomic,assign) BOOL networkActivityIndicatorEnabled ; ///> 是否显示转动的菊花
-@property (nonatomic,assign) BOOL cookieEnabled ;   ///> 是否使用Cookie
-@property (nonatomic,assign) BOOL debugLogEnable ;  ///> 是否打开调试信息
+/// 是否显示转动的菊花
+@property (nonatomic,assign) BOOL networkActivityIndicatorEnabled ;
+/// 是否使用Cookie
+@property (nonatomic,assign) BOOL cookieEnabled ;
+/// 是否打开调试信息
+@property (nonatomic,assign) BOOL debugLogEnable ;
 
-@property (nonatomic,readonly,copy) NSDictionary *allHTTPHeaderFields ;   ///> 设置的请求头信息
-- (void)setValue:(NSString *)value forHTTPHeaderField:(nonnull NSString *)field ; ///> 设置请求头信息
+/// 请求头信息
+@property (nonatomic,readonly,copy) NSDictionary *allHTTPHeaderFields ;
+/// 设置请求头信息
+- (void)setValue:(NSString *)value forHTTPHeaderField:(nonnull NSString *)field ;
 
-+ (instancetype)defaultConfig ;  ///> 默认设置
+/// 默认设置
++ (instancetype)defaultConfig ;
 
 @end
 
 @interface SINetworkManager : NSObject
+
+#pragma mark --- init
+- (instancetype)initWithConfig:(SINetworkConfig *)config ;
++ (instancetype)defaultManager ;
+
+#pragma mark --- 网络状态
+/// 网络是否可用
++ (BOOL)isNetwork ;
+/// 是不是蜂窝煤网络
++ (BOOL)isWWANNetwork ;
+/// 是否是WiFi网络
++ (BOOL)isWiFiNetwork ;
+/// 网络状态
++ (SINetworkStatusType)networkStatusType ;
+/// 网络状态发生改变,可能被多次调用
++ (void)networkStatusChageWithBlock:(SINetworkStatusBlock)block ;
+
+
 
 @end
 
--- SINetworkManagerDemo/SINetworkManager/SINetworkManager.m
+++ SINetworkManagerDemo/SINetworkManager/SINetworkManager.m
@@ -8,6 +8,164 @@
 
 #import "SINetworkManager.h"
 #import <YYCache/YYCache.h>
+#import <AFNetworking/AFNetworking.h>
+#import <AFNetworking/AFNetworkActivityIndicatorManager.h>
+
+#ifdef DEBUG
+#define SILog(...) printf("[%s] %s [第%d行]: %s\n", __TIME__ ,__PRETTY_FUNCTION__ ,__LINE__, [[NSString stringWithFormat:__VA_ARGS__] UTF8String])
+#else
+#define SILog(...)
+#endif
+
+
+
+NSString *const SINetworkStatusDidChangeNotification = @"SINetworkingReachabilityDidChangeNotification";
+
+
+#pragma mark - 来自YYKit的XML解析
+#pragma mark -
+
+@interface _YYXMLDictionaryParser : NSObject <NSXMLParserDelegate>
+@end
+
+@implementation _YYXMLDictionaryParser {
+    NSMutableDictionary *_root;
+    NSMutableArray *_stack;
+    NSMutableString *_text;
+}
+
+- (instancetype)initWithData:(NSData *)data {
+    self = super.init;
+    NSXMLParser *parser = [[NSXMLParser alloc] initWithData:data];
+    [parser setDelegate:self];
+    [parser parse];
+    return self;
+}
+
+- (instancetype)initWithString:(NSString *)xml {
+    NSData *data = [xml dataUsingEncoding:NSUTF8StringEncoding];
+    return [self initWithData:data];
+}
+
+- (NSDictionary *)result {
+    return _root;
+}
+
+#pragma mark - NSXMLParserDelegate
+
+#define XMLText @"_text"
+#define XMLName @"_name"
+#define XMLPref @"_"
+
+- (void)textEnd {
+    _text = [_text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]].mutableCopy;
+    if (_text.length) {
+        NSMutableDictionary *top = _stack.lastObject;
+        id existing = top[XMLText];
+        if ([existing isKindOfClass:[NSArray class]]) {
+            [existing addObject:_text];
+        } else if (existing) {
+            top[XMLText] = [@[existing, _text] mutableCopy];
+        } else {
+            top[XMLText] = _text;
+        }
+    }
+    _text = nil;
+}
+
+- (void)parser:(__unused NSXMLParser *)parser didStartElement:(NSString *)elementName namespaceURI:(__unused NSString *)namespaceURI qualifiedName:(__unused NSString *)qName attributes:(NSDictionary *)attributeDict {
+    [self textEnd];
+    
+    NSMutableDictionary *node = [NSMutableDictionary new];
+    if (!_root) node[XMLName] = elementName;
+    if (attributeDict.count) [node addEntriesFromDictionary:attributeDict];
+    
+    if (_root) {
+        NSMutableDictionary *top = _stack.lastObject;
+        id existing = top[elementName];
+        if ([existing isKindOfClass:[NSArray class]]) {
+            [existing addObject:node];
+        } else if (existing) {
+            top[elementName] = [@[existing, node] mutableCopy];
+        } else {
+            top[elementName] = node;
+        }
+        [_stack addObject:node];
+    } else {
+        _root = node;
+        _stack = [NSMutableArray arrayWithObject:node];
+    }
+}
+
+- (void)parser:(__unused NSXMLParser *)parser didEndElement:(__unused NSString *)elementName namespaceURI:(__unused NSString *)namespaceURI qualifiedName:(__unused NSString *)qName {
+    [self textEnd];
+    
+    NSMutableDictionary *top = _stack.lastObject;
+    [_stack removeLastObject];
+    
+    NSMutableDictionary *left = top.mutableCopy;
+    [left removeObjectsForKeys:@[XMLText, XMLName]];
+    for (NSString *key in left.allKeys) {
+        [left removeObjectForKey:key];
+        if ([key hasPrefix:XMLPref]) {
+            left[[key substringFromIndex:XMLPref.length]] = top[key];
+        }
+    }
+    if (left.count) return;
+    
+    NSMutableDictionary *children = top.mutableCopy;
+    [children removeObjectsForKeys:@[XMLText, XMLName]];
+    for (NSString *key in children.allKeys) {
+        if ([key hasPrefix:XMLPref]) {
+            [children removeObjectForKey:key];
+        }
+    }
+    if (children.count) return;
+    
+    NSMutableDictionary *topNew = _stack.lastObject;
+    NSString *nodeName = top[XMLName];
+    if (!nodeName) {
+        for (NSString *name in topNew) {
+            id object = topNew[name];
+            if (object == top) {
+                nodeName = name; break;
+            } else if ([object isKindOfClass:[NSArray class]] && [object containsObject:top]) {
+                nodeName = name; break;
+            }
+        }
+    }
+    if (!nodeName) return;
+    
+    id inner = top[XMLText];
+    if ([inner isKindOfClass:[NSArray class]]) {
+        inner = [inner componentsJoinedByString:@"\n"];
+    }
+    if (!inner) return;
+    
+    id parent = topNew[nodeName];
+    if ([parent isKindOfClass:[NSArray class]]) {
+        NSArray *parentAsArray = parent;
+        parent[parentAsArray.count - 1] = inner;
+    } else {
+        topNew[nodeName] = inner;
+    }
+}
+
+- (void)parser:(__unused NSXMLParser *)parser foundCharacters:(NSString *)string {
+    if (_text) [_text appendString:string];
+    else _text = [NSMutableString stringWithString:string];
+}
+
+- (void)parser:(__unused NSXMLParser *)parser foundCDATA:(NSData *)CDATABlock {
+    NSString *string = [[NSString alloc] initWithData:CDATABlock encoding:NSUTF8StringEncoding];
+    if (_text) [_text appendString:string];
+    else _text = [NSMutableString stringWithString:string];
+}
+
+#undef XMLText
+#undef XMLName
+#undef XMLPref
+@end
 
 
 #pragma mark --- SINetworkCache
@@ -75,6 +233,7 @@ static YYCache *_networkCache ;
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         config = [[self alloc]init] ;
+        
     });
     return config ;
 }
@@ -105,7 +264,111 @@ static YYCache *_networkCache ;
 @end
 
 #pragma mark ---- SINetworkManager
-@implementation SINetworkManager
+static SINetworkStatusType _currentNetworkStatus ;
+static AFHTTPSessionManager *_sessionManager;
+static NSMutableArray <NSURLSessionTask *>*_allSessionTask;
+static dispatch_semaphore_t _semaphore ;
+@implementation SINetworkManager{
+    SINetworkConfig *_config ;
+}
+
+#pragma mark --- 初始化
++ (void)load{
+    _allSessionTask = [NSMutableArray array] ;
+    _currentNetworkStatus = SINetworkStatusUnknow ;
+    _semaphore = dispatch_semaphore_create(1);
+    [[AFNetworkReachabilityManager sharedManager] startMonitoring] ;
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(networkStatusChanged:) name:AFNetworkingReachabilityDidChangeNotification object:nil] ;
+    
+}
+
+- (void)networkStatusChanged:(NSNotification *)noti{
+    NSNumber *number = [noti.userInfo objectForKey:AFNetworkingReachabilityNotificationStatusItem] ;
+    // 判断是什么类型
+    switch ([number integerValue]) {
+        case 0:
+            _currentNetworkStatus = SINetworkStatusNotReachable ;
+            break ;
+        case 1:
+            _currentNetworkStatus = SINetworkStatusReachableViaWWAN ;
+            break ;
+        case 2:
+            _currentNetworkStatus = SINetworkStatusReachableViaWiFi ;
+            break ;
+        default:
+            _currentNetworkStatus = SINetworkStatusUnknow ;
+            break;
+    }
+    
+    // 发送通知
+    [[NSNotificationCenter defaultCenter] postNotificationName:SINetworkStatusDidChangeNotification object:nil userInfo:noti.userInfo] ;
+}
+
+#pragma mark --- init
+- (instancetype)initWithConfig:(SINetworkConfig *)config{
+    if (self = [super init]) {
+        _config = config ;
+        // 所有请求公用一个AFHTTPSessionManager
+        _sessionManager = [[AFHTTPSessionManager alloc]initWithBaseURL:[NSURL URLWithString:_config.baseURL]] ;
+        _sessionManager.requestSerializer.timeoutInterval = _config.timeoutInterval ;
+        [_config.allHTTPHeaderFields enumerateKeysAndObjectsUsingBlock:^(id  _Nonnull key, id  _Nonnull obj, BOOL * _Nonnull stop) {
+            [_sessionManager.requestSerializer setValue:obj forHTTPHeaderField:key] ;
+        }] ;
+        _sessionManager.requestSerializer = config.requestSerializerType == 0 ? [AFHTTPRequestSerializer serializer] : [AFJSONRequestSerializer serializer];
+        _sessionManager.responseSerializer = [AFHTTPResponseSerializer serializer];
+        _sessionManager.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"application/json", @"text/html", @"text/json", @"text/plain", @"text/javascript", @"text/xml", @"image/*", nil];
+    }
+    return self ;
+}
+
+- (instancetype)init{
+    return [self initWithConfig:[SINetworkConfig defaultConfig]] ;
+}
+
++ (instancetype)defaultManager{
+    return [[self alloc]init] ;
+}
+
+#pragma mark --- 网络状态
++ (BOOL)isNetwork{
+    return [AFNetworkReachabilityManager sharedManager].reachable ;
+}
+
++ (BOOL)isWiFiNetwork{
+    return [AFNetworkReachabilityManager sharedManager].reachableViaWiFi ;
+}
+
++ (BOOL)isWWANNetwork{
+    return [AFNetworkReachabilityManager sharedManager].reachableViaWWAN ;
+}
+
++ (SINetworkStatusType)networkStatusType{
+    return _currentNetworkStatus ;
+}
+
++ (void)networkStatusChageWithBlock:(SINetworkStatusBlock)block{
+    [[AFNetworkReachabilityManager sharedManager] setReachabilityStatusChangeBlock:^(AFNetworkReachabilityStatus status) {
+        switch (status) {
+            case AFNetworkReachabilityStatusUnknown:
+                SILog(@"未知网络情况") ;
+                block ? block(SINetworkStatusUnknow) : nil ;
+                break;
+            case AFNetworkReachabilityStatusNotReachable:
+                SILog(@"无网络") ;
+                block ? block(SINetworkStatusNotReachable) : nil ;
+                break ;
+            case AFNetworkReachabilityStatusReachableViaWWAN :
+                SILog(@"蜂窝网络") ;
+                block ? block(SINetworkStatusReachableViaWWAN) : nil ;
+                break ;
+            case AFNetworkReachabilityStatusReachableViaWiFi:
+                SILog(@"WIFI网络") ;
+                block ? block(SINetworkStatusReachableViaWiFi) : nil ;
+                break ;
+        }
+    }] ;
+}
+
 @end
 
 
